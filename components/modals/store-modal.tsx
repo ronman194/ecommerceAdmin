3dbae4758c7d46b982df06067fbd3492df5ff6e0
FinalProjectRonMaor <139042047+FinalProjectRonMaor@users.noreply.github.com>
2023-08-02 11:57:40 +0300

    connect to prisma,planetscale and create api to create a store

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -1,14 +1,84 @@
 "use client";
 
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+
 import { ususeStoreModal } from "@/hooks/use-store-modal";
 import { Modal } from "../ui/modal";
+import { useForm } from "react-hook-form";
+import { Form, FormControl, FormField, FormItem, FormLabel } from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+
+const formSchema = z.object({
+    name: z.string().min(1),
+});
 
 export const StoreModal = () => {
     const storeModal = ususeStoreModal();
+    const [loading, setLoading] = useState(false);
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+        },
+    });
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        try {
+            setLoading(true);
+            const response = await axios.post('/api/stores', values);
+            toast.success("Store Created.");
+        } catch (error) {
+            console.log(error);
+            toast.error("Can't create a store");
+        } finally {
+            setLoading(false);
+        }
+    }
 
     return (
         <Modal title="Crete Store" description="Add a new store to manage" isOpen={storeModal.isOpen} onClose={storeModal.onClose}>
-            Create Store Form
-        </Modal>
+
+            <div>
+                <div className="space-y-4 py-2 pb-4">
+                    <Form {...form}>
+                        <form onSubmit={form.handleSubmit(onSubmit)}>
+                            <FormField
+                                control={form.control}
+                                name="name"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Name</FormLabel>
+                                        <FormControl>
+                                            <Input disabled={loading} placeholder="Store Name" {...field} />
+                                        </FormControl>
+                                    </FormItem>
+                                )}
+                            />
+                            <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+                                <Button
+                                    disabled={loading}
+                                    variant="outline"
+                                    onClick={storeModal.onClose}
+                                >
+                                    Cancel
+                                </Button>
+                                <Button
+                                    disabled={loading}
+                                    type="submit"
+                                >
+                                    Continue
+                                </Button>
+                            </div>
+                        </form>
+                    </Form>
+                </div>
+            </div>
+        </Modal >
     );
 }
