5ce9121f52ad0611da472ded49456ca20936c666
FinalProjectRonMaor <139042047+FinalProjectRonMaor@users.noreply.github.com>
2023-08-06 14:11:13 +0300

    orders page

---
+++ app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { OrderColumn, columns } from "./columns";
+import { DataTable } from "@/components/ui/data-table";
+
+interface OrderClientProps {
+    data: OrderColumn[];
+}
+
+export const OrderClient: React.FC<OrderClientProps> = ({ data }) => {
+    return (
+        <>
+            <Heading title={`Orders (${data.length})`} description="Manage orders of the store" />
+            <Separator />
+            <DataTable columns={columns} data={data} searchKey="products" />
+        </>
+    )
+}
